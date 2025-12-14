e269bf5804971ff8462b00e7b2938f862cb2432b
aditichauhan002 <aditichauhan95280@gmail.com>
2025-12-14 17:15:08 +0530

    Footer added to both files

--- index.js
+++ index.js
@@ -73,3 +73,15 @@ const filters = document.querySelectorAll(".section4 ul li");
       });
     });
   });
+
+
+
+
+  // ----------------------------------Footer--------------------------------------
+
+
+    fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
